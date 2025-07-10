0648cd794df1f480ab829a3871c2fcd8f72a3f0d
PARK TAEJIN <140797244+taejinn@users.noreply.github.com>
2025-07-10 23:34:14 +0900

    feat: start-spring-io

---
+++ src/test/java/com/example/maedeup/MaedeupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maedeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaedeupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
